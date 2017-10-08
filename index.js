10b69ed6772256df502c16226e7101a6bf0286e6
Webb Peng <webb.png@gmail.com>
2017-10-08 16:37:28 +0800

    smaller

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 
-const assign = require('object-assign');
 const _subs = '_subscribable_lite';
 
 const Subscribable = function Subscribable() {};
 
-assign(Subscribable.prototype, {
-	subscribe(fn) {
-		fn && (this[_subs] = (this[_subs] || []).concat(fn));
-		return () => this.unsubscribe(fn);
-	},
-	publish() {
-		(this[_subs] || []).forEach((fn) => fn(...arguments));
-	},
-	unsubscribe(fn) {
-		if (fn) { (this[_subs] || []).splice(this[_subs].indexOf(fn), 1); }
-		else { this[_subs] = []; }
-	},
-});
+Subscribable.prototype.subscribe = function subscribe(fn) {
+	fn && (this[_subs] = (this[_subs] || []).concat(fn));
+	return () => this.unsubscribe(fn);
+};
+
+Subscribable.prototype.publish = function publish() {
+	(this[_subs] || []).forEach((fn) => fn.apply(this, arguments));
+};
+
+Subscribable.prototype.unsubscribe = function unsubscribe(fn) {
+	if (fn) { (this[_subs] || []).splice(this[_subs].indexOf(fn), 1); }
+	else { this[_subs] = []; }
+};
 
 module.exports = Subscribable;
